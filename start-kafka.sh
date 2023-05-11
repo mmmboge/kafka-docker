d5e188166f28138aa75552a38a95926c2f4b0dbf
root <593493954@qq.com>
2023-05-11 08:53:33 +0800

    add docker-compose.yaml

--- start-kafka.sh
+++ start-kafka.sh
@@ -39,8 +39,11 @@ do
 
 done
 
+if [[ -z "$KAFKA_CLUSTER_ID" ]]; then
+    
+    export KAFKA_CLUSTER_ID="$(bin/kafka-storage.sh random-uuid)"
+fi
 
-KAFKA_CLUSTER_ID="$(bin/kafka-storage.sh random-uuid)"
 bin/kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c config/kraft/server.properties
 
 
